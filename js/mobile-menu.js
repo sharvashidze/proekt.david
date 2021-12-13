c821ac4cc0e5fe1c81ca9266d7a29edebeff2c47
beka sharvashidize <beka.sharvashidze@mail.ru>
2021-12-13 18:28:49 +0300

    2
    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -27,3 +27,22 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+$( document ).ready(function() {
+
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function () {
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+});
+$('body').on('click touchstart', function () {
+            const videoElement = document.getElementById('merivideo');
+            if (videoElement.playing) {
+                // video is already playing so do nothing
+            }
+            else {
+                // video is not playing
+                // so play video now
+                videoElement.play();
+            }
+    });
+});
